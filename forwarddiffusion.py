b6e9db67a0585c60d40d47d700a73c557d63d072
Matthew Weidman <weidman.matthew@gmail.com>
2024-10-25 21:10:31 -0700

    Put image noising in its own function

--- forwarddiffusion.py
+++ forwarddiffusion.py
@@ -13,18 +13,21 @@ alpha_bar_noise_schedule = [
 ]
 print(alpha_bar_noise_schedule)
 
+def noise_image(img, time_step):
+    noise = np.random.normal(size=(original_img.shape))
+    img = math.sqrt(alpha_bar_noise_schedule[time_step]) * original_img + \
+        math.sqrt(1-alpha_bar_noise_schedule[time_step]) * noise
+    # Clip within RGB bounds. According to https://arxiv.org/pdf/2102.09672, there
+    # are "singularities" near t=T if you let values get too close to 1, so we
+    # clip at 0.999. Don't ask me what singularities are.
+    return img.clip(min=0, max=0.999)
+
 original_img = np.asarray(Image.open('anakin_sand.jpg')) / 256
 ROW_LENGTH = 10
 fig, ax = plt.subplots(ncols=ROW_LENGTH, nrows=TOTAL_TIME_STEPS // ROW_LENGTH)
 ax = ax.flatten()
 for i in range(TOTAL_TIME_STEPS):
-    noise = np.random.normal(size=(original_img.shape))
-    img = math.sqrt(alpha_bar_noise_schedule[i]) * original_img + \
-        math.sqrt(1-alpha_bar_noise_schedule[i]) * noise
-    # Clip within RGB bounds. According to https://arxiv.org/pdf/2102.09672, there
-    # are "singularities" near t=T if you let values get too close to 1, so we
-    # clip at 0.999. Don't ask me what singularities are.
-    img = img.clip(min=0, max=0.999)
+    img = noise_image(original_img, i)
     ax[i].imshow(img)
     ax[i].set_axis_off()
 plt.show()
